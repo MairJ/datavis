19fd0726b126535685c5b75fd540e149b66bd4c1
MairJ <89141806+MairJ@users.noreply.github.com>
2023-08-03 10:01:12 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -15,7 +15,7 @@ st.dataframe(df)
 el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('Select Element', el_list)
 
-options = st.multiselect('select location', file_name_list, file_name_list[0])
+options = st.multiselect('select location', file_name_list)#, file_name_list[0])
 
 st.write('You selected:', options)
 
@@ -27,3 +27,14 @@ st.download_button(
     mime='text/csv',
 )
 """
+
+datasheet_list = file_name_list
+def plot_datasheets(el_x, el_y, datasheet_list):
+    for i in datasheet_list:
+        plt.scatter(data[el_x]/10000, data[el_y]/10000, label = i[:-4])
+    plt.xlabel(el_x + ' (wt%)')
+    plt.ylabel(el_y+ ' (wt%)')
+    plt.legend()
+    plt.show()
+
+plot_datasheets('Mg', 'Si', datasheet_list)
