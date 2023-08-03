4c9168850f7a9ccb2707bbd47105ca55c100ae23
MairJ <89141806+MairJ@users.noreply.github.com>
2023-08-03 12:17:41 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -19,15 +19,28 @@ el_list = df.columns.tolist()[27:80]
 el1 = st.selectbox('Select Element_X', el_list)
 el2 = st.selectbox('Select Element_Y', el_list)
 
-x = df[el1]/1e4  # Selecting the entire column for x axis
-y = df[el2]/1e4  # Selecting the entire column for y axis
+x = df[el1]  # Selecting the entire column for x axis
+y = df[el2]  # Selecting the entire column for y axis
 
-p = figure(title='Simple line example', x_axis_label=el1, y_axis_label=el2)
-p.circle(x, y, legend_label="Data Points")
-# Plot the mean and ±standard deviation of the y-axis value as lines.
 mean_y = np.mean(y)
-p.line(mean_y)
 std_y = np.std(y)
-p.line(std_y)
+
+# Calculate upper and lower bounds for ± standard deviation
+upper_bound = mean_y + std_y
+lower_bound = mean_y - std_y
+
+p = figure(title='Mean and ± Standard Deviation', x_axis_label=el1, y_axis_label=el2)
+
+# Plot the mean as a line
+p.line(x, mean_y, line_color='blue', legend_label='Mean')
+
+# Plot the upper bound of standard deviation as a line
+p.line(x, upper_bound, line_color='green', line_dash='dashed', legend_label='Upper Bound')
+
+# Plot the lower bound of standard deviation as a line
+p.line(x, lower_bound, line_color='red', line_dash='dashed', legend_label='Lower Bound')
+
+# Add data points to the plot
+p.circle(x, y, legend_label="Data Points")
 
 st.bokeh_chart(p, use_container_width=True)
