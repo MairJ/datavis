d5cea17de04ef1d5849fa1cf7e9fdb68c1da8f0b
MairJ <89141806+MairJ@users.noreply.github.com>
2023-08-03 12:28:29 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -11,41 +11,40 @@ for i in os.listdir():
 
 # st.write(file_name_list) # Test
 
-selected_file = st.multiselect('Select a CSV file', file_name_list)
-df = pd.read_csv(selected_file)
-st.dataframe(df)
+selected_files = st.multiselect('Select CSV files', file_name_list)
+if selected_files:
+    dfs = [pd.read_csv(file) for file in selected_files]
+    df = pd.concat(dfs, ignore_index=True)
+    st.dataframe(df)
 
+    el_list = df.columns.tolist()[27:80]
+    el1 = st.selectbox('Select Element_X', el_list)
+    el2 = st.selectbox('Select Element_Y', el_list)
 
+    x = df[el1] / 1e4  # Selecting the entire column for x axis and scaling by 1e4
+    y = df[el2] / 1e4  # Selecting the entire column for y axis and scaling by 1e4
 
+    std_choice = st.radio("Select Number of Standard Deviations", [1, 2, 3], index=1)
 
-el_list = df.columns.tolist()[27:80]
-el1 = st.selectbox('Select Element_X', el_list)
-el2 = st.selectbox('Select Element_Y', el_list)
+    mean_y = np.mean(y)
+    std_y = np.std(y)
 
-x = df[el1]/1e4  # Selecting the entire column for x axis
-y = df[el2]/1e4  # Selecting the entire column for y axis
+    # Calculate upper and lower bounds for ± standard deviation
+    upper_bound = mean_y + std_choice * std_y
+    lower_bound = mean_y - std_choice * std_y
 
-std_choice = st.radio("Select Number of Standard Deviations", [1, 2, 3], index=1)
+    p = figure(title='Mean and ± Standard Deviation', x_axis_label=el1, y_axis_label=el2)
 
-mean_y = np.mean(y)
-std_y = np.std(y)
+    # Plot the mean as a line
+    p.line(x, mean_y, line_color='blue', legend_label='Mean')
 
-# Calculate upper and lower bounds for ± standard deviation
-upper_bound = mean_y + std_choice * std_y
-lower_bound = mean_y - std_choice * std_y
+    # Plot the upper bound of standard deviation as a line
+    p.line(x, upper_bound, line_color='green', line_dash='dashed', legend_label='Upper Bound')
 
-p = figure(title='Mean and ± Standard Deviation', x_axis_label=el1, y_axis_label=el2)
+    # Plot the lower bound of standard deviation as a line
+    p.line(x, lower_bound, line_color='red', line_dash='dashed', legend_label='Lower Bound')
 
-# Plot the mean as a line
-p.line(x, mean_y, line_color='blue', legend_label='Mean')
+    # Add data points to the plot
+    p.circle(x, y, legend_label="Data Points")
 
-# Plot the upper bound of standard deviation as a line
-p.line(x, upper_bound, line_color='green', line_dash='dashed', legend_label='Upper Bound')
-
-# Plot the lower bound of standard deviation as a line
-p.line(x, lower_bound, line_color='red', line_dash='dashed', legend_label='Lower Bound')
-
-# Add data points to the plot
-p.circle(x, y, legend_label="Data Points")
-
-st.bokeh_chart(p, use_container_width=True)
+    st.bokeh_chart(p, use_container_width=True)
