eb5c15059939b87e21b443687a37f877ae5e2db2
MairJ <89141806+MairJ@users.noreply.github.com>
2023-08-03 09:43:33 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -7,13 +7,13 @@ file_name_list = []
 for i in os.listdir():
   if i.endswith('csv'):
     file_name_list.append(i)
-st.write(file_name_list)
+# st.write(file_name_list) # Test
 
-  #df = pd.read_csv('Bastar Craton.csv')
-  #st.dataframe(df)
+df = pd.read_csv('Bastar Craton.csv')
+st.dataframe(df)
 
-#el_list = df.columns.tolist()[27:80]
-#x_axis = st.selectbox('Select Element', el_list)
+el_list = df.columns.tolist()[27:80]
+x_axis = st.selectbox('Select Element', el_list)
 
-
+st.selectbox('select location', file_name_list)
 
