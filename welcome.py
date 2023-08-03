9593435a9de21d6605c069a06fc7a31eee56c204
MairJ <89141806+MairJ@users.noreply.github.com>
2023-08-03 09:55:16 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -15,7 +15,7 @@ st.dataframe(df)
 el_list = df.columns.tolist()[27:80]
 x_axis = st.selectbox('Select Element', el_list)
 
-option = st.multiselect('select location', file_name_list, file_name_list[0])
+options = st.multiselect('select location', file_name_list, file_name_list[0])
 
 st.download_button(
     label="Download data as CSV",
